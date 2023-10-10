4a4eea4099054cda818491a847e3936cbb69d71c
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-10-10 00:45:02 -0400

    Create ObjectData Script to be applied to every networked object

---
+++ Assets/My Modified Things/Scripts/ObjectData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectData : MonoBehaviour
+{
+
+    public int groupNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        groupNumber = 0;
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+    
+}
