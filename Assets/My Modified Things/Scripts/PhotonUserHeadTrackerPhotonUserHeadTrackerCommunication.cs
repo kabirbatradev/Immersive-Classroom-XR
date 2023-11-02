2660b315a1d2a9e23f4ade93d824392399060fe9
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-11-02 12:22:07 -0400

    created MyPhotonUserHeadTracker prefab with cloud communication script

---
+++ Assets/My Modified Things/Scripts/PhotonUserHeadTrackerPhotonUserHeadTrackerCommunication.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+
+public class PhotonUserHeadTrackerCommunication : MonoBehaviour, IPunObservable
+{
+
+    private PhotonView photonView;
+
+    // get the photon view of the gameobject
+    private void Start() {
+        photonView = GetComponent<PhotonView>();
+        // photonView.isMine() tells us if this object was instantiated locally or not
+    }
+
+    // this function is called every time this object tries to update itself
+    // if this object was instantiated locally (it is the local head), then it only sends data
+    // otherwise, this object must have been instantiated elsewhere (it is someone elses head), so it recieves data
+    // streaming happens many times per second (very fast)
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+
+        // if writing, then this object was instantiated locally (it is the local head)
+        if (stream.IsWriting) {
+
+            // write the head data of the head transform located in the UserHeadPositionTrackerManager
+                // this script has only 1 instance, and we passed in the "center eye anchor" aka the position of the head
+
+            Transform localHead = UserHeadPositionTrackerManager.Instance.localHeadTransform;
+
+            stream.SendNext(localHead.position);
+            stream.SendNext(localHead.eulerAngles);
+        }
+
+        // if reading, this object must have been instantiated elsewhere (it is someone elses head)
+        else {
+
+            // by recieving data on someone elses head, we can move around this current prefab itself
+                // if this prefab has a child object with an actual mesh, then we will see that mesh correspond to the head
+                // of another user
+            
+            gameObject.transform.position = (Vector3)stream.ReceiveNext();
+            gameObject.transform.eulerAngles = (Vector3)stream.ReceiveNext();
+        }
+
+    }
+}
--- Assets/My Modified Things/Scripts/UserHeadPositionTrackerManager.cs
+++ Assets/My Modified Things/Scripts/UserHeadPositionTrackerManager.cs
@@ -2,7 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UserHeadPositionTrackerManager : MonoBehaviour
+using PhotonPun = Photon.Pun;
+using PhotonRealtime = Photon.Realtime;
+
+public class UserHeadPositionTrackerManager : PhotonPun.MonoBehaviourPunCallbacks
 {
 
     // this class is a singleton so that the photon prefab for the head can get the head transform data from this script easily
@@ -12,5 +15,23 @@ public class UserHeadPositionTrackerManager : MonoBehaviour
 
     private void Awake() {
         Instance = this;
+
+        // if (Instance == null)
+        // {
+        //     Instance = this;
+        // }
+        // else
+        // {
+        //     Destroy(this);
+        // }
+    }
+
+
+    // this function is automatically called since our class extends PhotonPun.MonoBehaviourPunCallbacks
+    public override void OnJoinedRoom() {
+
+        // HeadPrefab exists in the resources folder so Photon should be able to access it
+        Photon.Pun.PhotonNetwork.Instantiate("MyPhotonUserHeadTracker", Vector3.zero, Quaternion.identity);
+
     }
 }
