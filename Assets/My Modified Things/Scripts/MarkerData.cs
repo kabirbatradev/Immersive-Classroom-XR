2edea75641bf9e34aeb0ffbbdc32d5489e6f514c
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2024-02-21 23:11:23 -0500

    new MarkerPrefab with MarkerData script

---
+++ Assets/My Modified Things/Scripts/MarkerData.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkerData : MonoBehaviour
+{
+
+    struct MarkerDataStruct {
+        public int row;
+        public int column;
+        public int totalColumnsInThisRow;
+    }
+
+
+
+    // void Start() {
+        
+    // }
+
+    // void Update() {
+        
+    // }
+
+
+    public int GetRow() {
+        return -1;
+    }
+
+    public int GetColumn() {
+        return -1;
+    }
+
+    public int GetTotalColumnsInThisRow() {
+        return -1;
+    }
+
+
+
+
+}
