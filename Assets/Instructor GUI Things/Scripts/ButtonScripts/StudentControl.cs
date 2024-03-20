85de787df11302c7fb0e5519f18bd138ec035a12
ZRZ1127 <zr.z1127@gmail.com>
2024-03-20 15:08:17 -0400

    Updated split student functions

--- Assets/Instructor GUI Things/Scripts/ButtonScripts/StudentControl.cs
+++ Assets/Instructor GUI Things/Scripts/ButtonScripts/StudentControl.cs
@@ -1,29 +1,127 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
-public class SplitStudentController : MonoBehaviour
+public class StudentnControl : MonoBehaviour
 {
-    // call back func for splitting student into individual groups
-    public void splitIndividual()
+    public GameObject[] markers;
+    public GameObject[] studentsHeads;
+
+    // ------------ Button Functions ------------
+    // Used to split students into individual groups
+    public void SplitIndividual()
     {
-        // code here to tell the server splitting into groups of 1 student
-        Debug.Log("split individual button pressed");
-        InstructorCloudFunctions.Instance.SetStudentsIntoIndividualGroups();
+        // Find all markers and students
+        // This is set during runtime so that the markers and students are found after they are created
+        markers = GameObject.FindGameObjectsWithTag("SeatMarker");
+        studentsHeads = GameObject.FindGameObjectsWithTag("PlayerHead");
+        int[] groupAssignment = new int[studentsHeads.Length];
+        int group = 1;
+        Debug.Log("Len of studentsHeads: " + studentsHeads.Length);
+        foreach (GameObject student in studentsHeads)
+        {
+            // append current group to groupAssignment
+            groupAssignment[group - 1] = group;
+            group++;
+        }
+        InstructorCloudFunctions.Instance.AssignEachPlayerHeadToSpecificGroupNumber(studentsHeads, groupAssignment);
     }
 
-    public void splitPair()
+    // Used to split students into rows
+    public void SplitRow()
     {
-        // code here to tell the server splitting into groups of 2 students
-        Debug.Log("split into pairs button pressed");
-        InstructorCloudFunctions.Instance.SetStudentsIntoGroupsOfTwo();
+        markers = GameObject.FindGameObjectsWithTag("SeatMarker");
+        studentsHeads = GameObject.FindGameObjectsWithTag("PlayerHead");
+        int[] groupAssignment = new int[studentsHeads.Length];
+        int index = 0;
+        foreach (GameObject student in studentsHeads)
+        {
+            int row = findRow(student);
+            Debug.Log("Row: " + row);
+            groupAssignment[index] = row + 1;
+            index++;
+        }
+        InstructorCloudFunctions.Instance.AssignEachPlayerHeadToSpecificGroupNumber(studentsHeads, groupAssignment);
     }
 
-    public void splitAll()
+    // Used to split students int two rows
+    public void SplitDoubleRow()
     {
-        // code here to tell the server to have a group of all students
-        Debug.Log("all students 1 group button pressed");
-        InstructorCloudFunctions.Instance.SetAllStudentsGroupOne();
+        markers = GameObject.FindGameObjectsWithTag("SeatMarker");
+        studentsHeads = GameObject.FindGameObjectsWithTag("PlayerHead");
+        int[] groupAssignment = new int[studentsHeads.Length];
+        int index = 0;
+        foreach (GameObject student in studentsHeads)
+        {
+            int row = findRow(student);
+            Debug.Log("Row: " + row);
+            groupAssignment[index] = int(row / 2) + 1;
+            index++;
+        }
+        InstructorCloudFunctions.Instance.AssignEachPlayerHeadToSpecificGroupNumber(studentsHeads, groupAssignment);
     }
+
+    // Used to split students into squares of 4
+    public void SplitFour()
+    {
+        markers = GameObject.FindGameObjectsWithTag("SeatMarker");
+        studentsHeads = GameObject.FindGameObjectsWithTag("PlayerHead");
+        int[] groupAssignment = new int[studentsHeads.Length];
+        int group = 1;
+        Debug.Log("Len of studentsHeads: " + studentsHeads.Length);
+        foreach (GameObject student in studentsHeads)
+        {
+            // append current group to groupAssignment
+            groupAssignment[group - 1] = group;
+            group++;
+        }
+        InstructorCloudFunctions.Instance.AssignEachPlayerHeadToSpecificGroupNumber(studentsHeads, groupAssignment);
+    }
+    // ------------ Button Functions ------------
+
+
+
+    // ------------ Helper Functions ------------
+    // Find the closest marker to the student and return the row
+    public int findRow(GameObject studentsHeads)
+    {
+        float minDist = Mathf.Infinity;
+        GameObject closestMarker = null;
+        markers = GameObject.FindGameObjectsWithTag("SeatMarker");
+        foreach (GameObject marker in markers)
+        {
+            float dist = Vector3.Distance(studentsHeads.transform.position, marker.transform.position);
+            if (dist < minDist)
+            {
+                minDist = dist;
+                closestMarker = marker;
+            }
+        }
+        return closestMarker.GetComponent<SeatMarkerData>().GetRow();
+    }
+
+    // Find the closest marker to the student and return the column
+    public int findCol(GameObject studentsHeads)
+    {
+        float minDist = Mathf.Infinity;
+        GameObject closestMarker = null;
+        markers = GameObject.FindGameObjectsWithTag("SeatMarker");
+        foreach (GameObject marker in markers)
+        {
+            float dist = Vector3.Distance(studentsHeads.transform.position, marker.transform.position);
+            if (dist < minDist)
+            {
+                minDist = dist;
+                closestMarker = marker;
+            }
+        }
+        return closestMarker.GetComponent<SeatMarkerData>().GetColumn();
+    }
+
+    // Find the closest marker to the student and return the group
+    public int findFourGroup(GameObject studentsHeads)
+    {
+        return 1;
+    }
+    // ------------ Helper Functions ------------
 }
--- Assets/SplitStudent/Scripts/SplitStudent.cs
+++ Assets/SplitStudent/Scripts/SplitStudent.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class SplitStudent : MonoBehaviour
 {
     public GameObject[] markers;
-    public GameObject[] students;
+    public GameObject[] studentsHeads;
+    public int[] groupAssignment;
 
     // ------------ Button Functions ------------
     // Used to split students into individual groups
@@ -13,57 +14,45 @@ public class SplitStudent : MonoBehaviour
     {
         // Find all markers and students
         // This is set during runtime so that the markers and students are found after they are created
-        markers = GameObject.FindGameObjectsWithTag("Marker");
-        students = GameObject.FindGameObjectsWithTag("FakeStudent");
+        markers = GameObject.FindGameObjectsWithTag("SeatMarker");
+        studentsHeads = GameObject.FindGameObjectsWithTag("PlayerHead");
         int group = 1;
-        foreach (GameObject student in students)
+        foreach (GameObject student in studentsHeads)
         {
-            student.GetComponent<FakeStudent>().group = group;
+            // append current group to groupAssignment
+            groupAssignment[group - 1] = group;
             group++;
         }
-        // Set the color of the students
-        // This is used for debugging purposes
-        // Remove this for production
-        setStudentColor(36);
+        InstructorCloudFunctions.Instance.AssignEachPlayerHeadToSpecificGroupNumber(studentsHeads, groupAssignment);
     }
 
     // Used to split students into rows
     public void SplitRow()
     {
         markers = GameObject.FindGameObjectsWithTag("Marker");
-        students = GameObject.FindGameObjectsWithTag("FakeStudent");
-        foreach (GameObject student in students)
+        studentsHeads = GameObject.FindGameObjectsWithTag("FakeStudent");
+        foreach (GameObject student in studentsHeads)
         {
             int row = findRow(student);
             student.GetComponent<FakeStudent>().group = row + 1;
         }
-        setStudentColor(6);
     }
 
     // Used to split students int two rows
     public void SplitDoubleRow()
     {
         markers = GameObject.FindGameObjectsWithTag("Marker");
-        students = GameObject.FindGameObjectsWithTag("FakeStudent");
-        foreach (GameObject student in students)
+        studentsHeads = GameObject.FindGameObjectsWithTag("FakeStudent");
+        foreach (GameObject student in studentsHeads)
         {
             int row = findRow(student);
             student.GetComponent<FakeStudent>().group = (row / 2) + 1;
         }
-        setStudentColor(3);
     }
 
     // Used to split students into squares of 4
     public void SplitFour()
     {
-        markers = GameObject.FindGameObjectsWithTag("Marker");
-        students = GameObject.FindGameObjectsWithTag("FakeStudent");
-        foreach (GameObject student in students)
-        {
-            int group = findFourGroup(student);
-            student.GetComponent<FakeStudent>().group = group;
-        }
-        setStudentColor(9);
     }
     // ------------ Button Functions ------------
 
@@ -71,13 +60,13 @@ public class SplitStudent : MonoBehaviour
 
     // ------------ Helper Functions ------------
     // Find the closest marker to the student and return the row
-    public int findRow(GameObject student)
+    public int findRow(GameObject studentsHeads)
     {
         float minDist = Mathf.Infinity;
         GameObject closestMarker = null;
         foreach (GameObject marker in markers)
         {
-            float dist = Vector3.Distance(student.transform.position, marker.transform.position);
+            float dist = Vector3.Distance(studentsHeads.transform.position, marker.transform.position);
             if (dist < minDist)
             {
                 minDist = dist;
@@ -88,13 +77,13 @@ public class SplitStudent : MonoBehaviour
     }
 
     // Find the closest marker to the student and return the column
-    public int findCol(GameObject student)
+    public int findCol(GameObject studentsHeads)
     {
         float minDist = Mathf.Infinity;
         GameObject closestMarker = null;
         foreach (GameObject marker in markers)
         {
-            float dist = Vector3.Distance(student.transform.position, marker.transform.position);
+            float dist = Vector3.Distance(studentsHeads.transform.position, marker.transform.position);
             if (dist < minDist)
             {
                 minDist = dist;
@@ -105,13 +94,13 @@ public class SplitStudent : MonoBehaviour
     }
 
     // Find the closest marker to the student and return the group
-    public int findFourGroup(GameObject student)
+    public int findFourGroup(GameObject studentsHeads)
     {
         float minDist = Mathf.Infinity;
         GameObject closestMarker = null;
         foreach (GameObject marker in markers)
         {
-            float dist = Vector3.Distance(student.transform.position, marker.transform.position);
+            float dist = Vector3.Distance(studentsHeads.transform.position, marker.transform.position);
             if (dist < minDist)
             {
                 minDist = dist;
@@ -121,23 +110,4 @@ public class SplitStudent : MonoBehaviour
         return closestMarker.GetComponent<Marker>().GetGroup();
     }
     // ------------ Helper Functions ------------
-
-
-
-    // ------------ Debug Functions ------------
-    public void setStudentColor(int totalGroup)
-    {
-        Color[] colors = new Color[(int)totalGroup];
-        for (int i = 0; i < totalGroup; i++)
-        {
-            colors[i] = Random.ColorHSV();
-        }
-
-        foreach (GameObject student in students)
-        {
-            int group = student.GetComponent<FakeStudent>().group;
-            student.GetComponent<Renderer>().material.color = colors[group - 1];
-        }
-    }
-    // ------------ Debug Functions ------------
 }
