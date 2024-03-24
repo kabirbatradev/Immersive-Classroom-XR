4eb4ddf57b1ee6f120ad04f26c9009382ab82202
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2024-03-23 22:59:06 -0400

    show passthrough anyway if walls and ceiling have not been lowered at all

--- Assets/My Modified Things/Theater Stuff/PassthroughToSkyboxController.cs
+++ Assets/My Modified Things/Theater Stuff/PassthroughToSkyboxController.cs
@@ -16,17 +16,26 @@ public class PassthroughToSkyboxController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-
         // if a ceiling object exists, then show Skybox; otherwise show passthrough
-        bool exists = null != GameObject.FindWithTag("CeilingMesh");
+        bool wallObjectsExist = null != GameObject.FindWithTag("CeilingMesh");
 
         // OVRCameraRig ovrCameraRig = GameObject.Find("OVRCameraRig").GetComponent<OVRCameraRig>();
         // var centerCamera = ovrCameraRig.centerEyeAnchor.GetComponent<Camera>();
-        if (exists) {
-            // image skybox
-            centerCamera.clearFlags = CameraClearFlags.Skybox;
+        if (wallObjectsExist) {
+            // show passthrough anyway if walls and ceiling have not been lowered at all
+            if (StreamTheaterModeData.Instance.wallLoweredPercentage == 0
+                    && StreamTheaterModeData.Instance.ceilingRemovedPercentage == 0) {
+                
+                centerCamera.clearFlags = CameraClearFlags.SolidColor;
+            }
+            else {
+                // show the skybox if the walls/ceiling have been lowered
+                centerCamera.clearFlags = CameraClearFlags.Skybox;
+            }
+
+            
         }
+        // fallback for when wall objects don't exist
         else {
             // passthrough
             centerCamera.clearFlags = CameraClearFlags.SolidColor;
