ddf1d8bc3c44be4bc085e16fd80f8dc5eaf8de6d
Kabir Batra <kabirbatraa@gmail.com>
2024-04-17 15:35:02 -0400

    make the HeadSphere transparent using new material and set alpha, test in test scene

--- Assets/Student XR/Scripts/UserHeadSphereController.cs
+++ Assets/Student XR/Scripts/UserHeadSphereController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 using PhotonPun = Photon.Pun;
@@ -8,12 +9,14 @@ public class UserHeadSphereController : MonoBehaviour
 {
 
 
-    private Renderer headRenderer;
+    private Renderer headSphereRenderer;
+    private PhotonView headTrackerPhotonView;
 
     // Start is called before the first frame update
     void Start()
     {
-        headRenderer = GetComponent<Renderer>();
+        headSphereRenderer = GetComponent<Renderer>();
+        headTrackerPhotonView = transform.parent.gameObject.GetPhotonView();
     }
 
     // Update is called once per frame
@@ -25,8 +28,8 @@ public class UserHeadSphereController : MonoBehaviour
         // renderer.material.color = Color.red;
 
 
-        GameObject headTracker = transform.parent.gameObject;
-        var ownerPlayer = headTracker.GetComponent<PhotonPun.PhotonView>().Owner;
+        // GameObject headTracker = transform.parent.gameObject;
+        var ownerPlayer = headTrackerPhotonView.Owner;
 
         int groupNumber = SharedAnchorControlPanelAdditionalFunctions.defaultGroupNumber;
         if (ownerPlayer.CustomProperties.ContainsKey("groupNumber")) {
@@ -41,9 +44,11 @@ public class UserHeadSphereController : MonoBehaviour
         float hue = (groupNumber % 6) / 6.0f; // max group number is 6; if more than 6, then the color cycles back
         // i chose 6 because the spacing between colors is really nice!
 
-        headRenderer.material.color = Color.HSVToRGB(hue, 1.0f, 1.0f);
-        // renderer.material.color = Color.HSVToRGB(0.5f, 1.0f, 1.0f); // this does work
-
+        // renderer.material.color = Color.HSVToRGB(0.5f, 1.0f, 1.0f); // this does work yay
+        Color c = Color.HSVToRGB(hue, 1.0f, 1.0f);
+        // set the color alpha to 0.3
+        c.a = 0.3f;
+        headSphereRenderer.material.color = c;
 
 
     }
