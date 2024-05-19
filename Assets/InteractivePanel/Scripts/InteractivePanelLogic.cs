ce02eb05ae33c85a58c644b87c7c886033e0ba95
Kabir Batra <kabirbatraa@gmail.com>
2024-05-19 13:36:22 -0700

    Revert "Merge branch 'Ellen' into test-panel-and-hands"
    This reverts commit b259b0a10a5ca5d2814d037320ad30960c35717f, reversing
    changes made to 27305dece363525d3863c1069d402621c0934595.

--- Assets/InteractivePanel/Scripts/InteractivePanelLogic.cs
+++ Assets/InteractivePanel/Scripts/InteractivePanelLogic.cs
@@ -5,53 +5,35 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEngine.UI;
 
-[System.Serializable]
+public class QuizItem
+{
+    public int Index { get; set; }
+    public string Question { get; set; }
+    public string[] Options { get; set; }
+    
+    public string CorrectAnswer { get; set; }
+}
+
 public class QuizContainer
 {
     public QuizItem[] Questions;
 }
 
-[System.Serializable]
-public class QuizItem
-{
-    public int Index;
-    public string Question;
-    public string[] Options;
-    public string CorrectAnswer;
-}
-
-
 public class InteractivePanelLogic : MonoBehaviour
 {
-    [SerializeField] private GameObject boundObject;
     [SerializeField] private TMP_Text titleText;
     [SerializeField] private TMP_Text questionText;
-    [SerializeField] private Toggle raiseHand;
     [SerializeField] private GameObject optionPrefab;
     [SerializeField] private ToggleGroup toggleGroup;
-
-    public Sprite raiseHandRaised;
-    public Sprite raiseHandNotRaised;
     
-    public Color optionOnColor;
-    public Color optionOffColor;
-
-    public Color optionCorrectColor;
-    public Color optionWrongColor;
-
-    private QuizContainer qc;
-    private QuizItem qi;
-
+    public Color onColor;
+    public Color offColor;
+    // Start is called before the first frame update
     void Start()
     {
-        raiseHand.onValueChanged.AddListener(UpdateRaiseHandStatus);
-        TextAsset file = Resources.Load("Quiz") as TextAsset;
-        qc = LoadQuiz(file);
-        qi = qc.Questions[0];
-        // qi = GenerateDateQuestion();
+        QuizItem qi = GenerateDateQuestion();
         Shuffle(qi.Options);
         UpdateQuestion(qi);
-        UpdateClippingBounds();
     }
 
     private void UpdateQuestion(QuizItem qi)
@@ -62,10 +44,6 @@ public class InteractivePanelLogic : MonoBehaviour
         for (int i = 0; i < qi.Options.Length; i++)
         {
             GameObject temp = Instantiate(optionPrefab, transform);
-            // Calculate the desired sibling index to make it the second-to-last child
-            int secondToLastIndex = Mathf.Max(transform.childCount - 2, 0);
-            // Set the new object's sibling index
-            temp.transform.SetSiblingIndex(secondToLastIndex);
             temp.GetComponent<Toggle>().group = toggleGroup;
             temp.GetComponent<Toggle>().onValueChanged.AddListener(HandleToggleValueChanged);
             temp.GetComponentInChildren<Text>().text = qi.Options[i];
@@ -76,9 +54,13 @@ public class InteractivePanelLogic : MonoBehaviour
     {
         foreach (Toggle toggle in toggleGroup.GetComponentsInChildren<Toggle>())
         {
-            if (toggle.group == toggleGroup)
+            if (toggle.isOn)
             {
-                toggle.GetComponentInChildren<Image>().color = toggle.isOn ? optionOnColor : optionOffColor;
+                toggle.GetComponentInChildren<Image>().color = onColor;
+            }
+            else
+            {
+                toggle.GetComponentInChildren<Image>().color = offColor;
             }
         }
     }
@@ -95,31 +77,18 @@ public class InteractivePanelLogic : MonoBehaviour
             }
         }
     }
-
-    public void GradeQuestion()
-    {
-        foreach (Toggle toggle in toggleGroup.GetComponentsInChildren<Toggle>())
-        {
-            if (toggle.group == toggleGroup)
-            {
-                if (toggle.isOn)
-                {
-                    bool isCorrect = toggle.GetComponentInChildren<Text>().text == qi.CorrectAnswer;
-                    toggle.GetComponentInChildren<Image>().color = isCorrect ? optionCorrectColor : optionWrongColor;
-                }
-            }
-        }
-    }
     
     // Reads a JSON file and returns a QuizContainer object
-    public QuizContainer LoadQuiz(TextAsset file)
+    public QuizContainer LoadQuiz(string filePath)
     {
         try
         {
             // Read all text from the file
-            string jsonContent = file.ToString();
+            string jsonContent = File.ReadAllText(filePath);
+
             // Deserialize the JSON content to a QuizContainer object
             QuizContainer quiz = JsonUtility.FromJson<QuizContainer>(jsonContent);
+
             return quiz;
         }
         catch (Exception e)
@@ -164,33 +133,4 @@ public class InteractivePanelLogic : MonoBehaviour
             options[j] = temp;
         }
     }
-
-    private void UpdateRaiseHandStatus(bool isOn)
-    {
-        raiseHand.GetComponent<Image>().sprite = isOn ? raiseHandRaised : raiseHandNotRaised;
-        if (isOn)
-        {
-            // Other logic of calling out to the instructor
-        }
-    }
-    
-    // Update clipping bounds so that the cursor isn't shown outside of UI panel
-    private void UpdateClippingBounds()
-    {
-        LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>());
-        // Get the corners of the RectTransform in world space
-        Vector3[] corners = new Vector3[4];
-        GetComponent<RectTransform>().GetWorldCorners(corners);
-
-        // Calculate the bounding box that contains all corners
-        Bounds bounds = new Bounds(corners[0], Vector3.zero);
-        for (int i = 1; i < corners.Length; i++)
-        {
-            bounds.Encapsulate(corners[i]);
-        }
-
-        Vector3 lossyScale = boundObject.transform.lossyScale;
-        boundObject.transform.localScale = new Vector3(bounds.size.x / lossyScale.x, bounds.size.y / lossyScale.y, boundObject.transform.localScale.z);
-    }
 }
-
